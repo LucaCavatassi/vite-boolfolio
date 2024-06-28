4fa4f39f9c1c608f4628972c47308db16e1fab60
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-28 15:32:48 +0200

    feat: printed technology into card

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,18 @@ export default {
   components: {
     ProjectCard
   },
-
+  
   created () {
-      axios.get("http://127.0.0.1:8000/api/projects").then((resp)=> {
-        this.projects = resp.data.results;
+    axios.get("http://127.0.0.1:8000/api/projects").then((resp)=> {
+      this.projects = resp.data.results;
+      console.log(this.projects[1].technologies);
       })
   }
 }
 </script>
 
 <template>
+  <h1 class="text-center py-3">I miei progetti!</h1>
   <div class="container">
     <ProjectCard :projects="projects"/>
   </div>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,25 +1,30 @@
 <script>
-    export default {
-        props: {
-            projects: Object
-        }
+export default {
+    props: {
+        projects: Object
     }
+
+}
 </script>
 
 <template>
     <div class="row">
-      <div class="col-3" v-for="project in projects">
-        <div class="card">
-          <div class="card-body">
-            <h1> {{ project.title }}</h1>
-          </div>
+        <div class="col d-flex justify-content-center mb-3" v-for="project in projects">
+            <div class="card h-100" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                    <h6>Tecnologie usate</h6>
+                    <div v-if="project.technologies.length  === 0">
+                        <p>Nessuna tecnologia indicata.</p>
+                    </div>
+                    <div v-else>
+                        <p v-for="technology in project.technologies" class="card-text">{{technology.name}}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-
-
 </template>
 
-<style>
-
-</style>
+<style></style>
