d35d367a42e06a64731b1a29ae9792c23254869d
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-28 14:46:20 +0200

    feat: axios call to back-end project and printing of title in page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script>
 import axios from "axios"
+export default {
+  data () {
+    return {
+      projects: [],
+    }
+  },
 
+  created () {
+      axios.get("http://127.0.0.1:8000/api/projects").then((resp)=> {
+        this.projects = resp.data.results;
+      })
+  }
+}
 </script>
 
 <template>
-<h1>HELLO</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-3" v-for="project in projects">
+        <div class="card">
+          <div class="card-body">
+            <h1> {{ project.title }}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
