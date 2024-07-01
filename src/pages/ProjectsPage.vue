d723dde34b32a0cc9f0e1bed4fca2fae26bde7ba
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-01 18:25:36 +0200

    feat: active color on click paginator

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -10,6 +10,10 @@ export default {
             projects: [],
             lastPage: [],
             curPage: 1,
+            perPage:[],
+            total:[],
+            from: [],
+            to: []
         }
     },
 
@@ -28,8 +32,13 @@ export default {
                     page: this.curPage
                 },
             }).then((resp) => {
-            this.projects = resp.data.results.data;
-            this.lastPage = resp.data.results.last_page;
+                this.projects = resp.data.results.data;
+                this.lastPage = resp.data.results.last_page;
+                this.perPage = resp.data.results.per_page;
+                this.total = resp.data.results.total;
+                this.from = resp.data.results.from;
+                this.to = resp.data.results.to;
+
             })
         },
         changePage(page) {
@@ -57,20 +66,25 @@ export default {
     <div class="container d-flex gap-2">
         <div class="row">
             <ProjectCard :projects="projects" />
-            <nav aria-label="Page navigation example">
+            <nav aria-label="Page navigation example" class="d-flex align-items-center justify-content-between">
                 <ul class="pagination">
+
                     <li class="page-item">
                         <a class="page-link" href="#" @click="firstPage" aria-label="Previous">
                             <span aria-hidden="true">&laquo;</span>
                         </a>
                     </li>
-                    <li class="page-item" v-for="page in lastPage"><a class="page-link" @click="changePage(page)" href="#">{{ page }}</a></li>
+
+                    <li class="page-item" :class="curPage === page ? 'active' : ''" v-for="page in lastPage"><a class="page-link" @click="changePage(page)" href="#">{{ page }}</a></li>
+
                     <li class="page-item">
                         <a class="page-link" href="#" @click="latestPage" aria-label="Next">
                             <span aria-hidden="true">&raquo;</span>
                         </a>
                     </li>
+
                 </ul>
+                <p class="fs-6">Stai vedendo {{ from }} - {{ to }} progetti di {{ total }}</p>
             </nav>
         </div>
     </div>
